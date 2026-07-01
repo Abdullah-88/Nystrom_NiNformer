5d2a50ac974dbf6528668876adf5b52ef001e038
Abdullah Nazhat Abdullah <abdullah.nazhat@gmail.com>
2026-07-02 01:39:52 +0300

    Update train.py

--- train.py
+++ train.py
@@ -12,8 +12,7 @@ from nystromninformer import NystromNiNformer
 # data transforms
 
 transform = Compose([
-RandomCrop(32, padding=4),
-RandomHorizontalFlip(), 
+ 
 ToTensor(),
 Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
 
